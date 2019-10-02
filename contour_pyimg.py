5639933b0cab3c078b46f2370bcf9c78226ad746
SRIJAN SINGH <41985103+srijansingh53@users.noreply.github.com>
2019-10-02 20:40:34 +0530

    contour_detect added

---
+++ contour_pyimg.py
@@ -0,0 +1,40 @@
+import imutils
+import cv2
+
+
+
+image = cv2.imread("hand3.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5,5), 0)
+
+# Thresholding the image
+
+thresh = cv2.threshold(gray, 45,225, cv2.THRESH_BINARY)[1]
+thresh = cv2.erode(thresh, None, iterations=2)
+thresh = cv2.dilate(thresh, None, iterations=2)
+cv2.imshow('thresh',thresh)
+
+# Findind Contours of max area
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+c = max(cnts, key=cv2.contourArea)
+
+# determine the most extreme points along the contour
+extLeft = tuple(c[c[:, :, 0].argmin()][0])
+extRight = tuple(c[c[:, :, 0].argmax()][0])
+extTop = tuple(c[c[:, :, 1].argmin()][0])
+extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+# draw the outline of the object, then draw each of the
+# extreme points, where the left-most is red, right-most
+# is green, top-most is blue, and bottom-most is teal
+cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
+cv2.circle(image, extLeft, 8, (0, 0, 255), -1)
+cv2.circle(image, extRight, 8, (0, 255, 0), -1)
+cv2.circle(image, extTop, 8, (255, 0, 0), -1)
+cv2.circle(image, extBot, 8, (255, 255, 0), -1)
+ 
+# show the output image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
